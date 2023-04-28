9d3736045f908e8ebe83ec0a771a8c713f7a6978
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-04-29 00:20:53 +0530

    Transition Added

--- src/Pages/HomePage/Contact/Contact.jsx
+++ src/Pages/HomePage/Contact/Contact.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./ContactStyle.css";
 import { IoMdCall } from "react-icons/io";
 import { HiOutlineMail } from "react-icons/hi";
 import { BiMap } from "react-icons/bi";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Contact = () => {
+  useEffect(()=>{
+    Aos.init({duration: 800});
+  },[]);
   return (
     <section id="contact" className="contact-sec">
-      <h1>Contact Us</h1>
+      <h1 data-aos="fade-right">Contact Us</h1>
       <div className="contact-container">
-        <div className="left-contact">
+        <div data-aos="flip-up" className="left-contact">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14253.622450778397!2d83.4331276!3d26.7314295!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39915ca3e2aa136b%3A0xc039bdf0211338a9!2sMadan%20Mohan%20Malaviya%20University%20Of%20Technology!5e0!3m2!1sen!2sin!4v1682628507785!5m2!1sen!2sin"
             allowFullScreen
@@ -17,7 +22,7 @@ const Contact = () => {
             referrerPolicy="no-referrer-when-downgrade"
           />
         </div>
-        <div className="right-contact">
+        <div data-aos="fade-up" className="right-contact">
           <form action method="post">
             <div className="row">
               <div className="row_details">
@@ -52,7 +57,7 @@ const Contact = () => {
           </form>
         </div>
       </div>
-      <div className="details-container">
+      <div data-aos="fade-up" className="details-container">
         <a href="">
           <IoMdCall /> 8095d656{" "}
         </a>
