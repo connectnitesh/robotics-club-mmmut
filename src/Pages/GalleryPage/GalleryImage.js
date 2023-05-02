17187ae3de4f2b0014922639eff2ac0086a354a6
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-05-03 00:21:28 +0530

    Gallery Photos added

--- src/Pages/GalleryPage/GalleryImage.js
+++ src/Pages/GalleryPage/GalleryImage.js
@@ -1,35 +1,65 @@
-import img1 from '../../assets/images/gallery/rcian.jpg'
-import img2 from '../../assets/images/gallery/rcian.jpg'
-import img3 from '../../assets/images/gallery/rcian.jpg'
-import img4 from '../../assets/images/gallery/rcian.jpg'
-
 
 const GalleryImage = [
     {
-       src: img1,
+       src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052904/Gallery/1_cook0l.jpg',
     },
     {
-       src: img2,
+       src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052907/Gallery/9_guujqf.jpg',
     },
     {
-       src: img3,
+       src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052907/Gallery/8_pmyv13.jpg',
     },
     {
-       src: img4,
+       src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052906/Gallery/6_gr7y6u.jpg',
     },
     {
-       src: img1,
+       src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052906/Gallery/22_d269ho.jpg',
     },
     {
-       src: img2,
+       src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052906/Gallery/7_uxiakd.jpg',
     },
     {
-       src: img3,
+       src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052906/Gallery/5_g2suao.jpg',
     },
     {
-       src: img4,
+       src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052906/Gallery/20_bt99tf.jpg',
     },
-
+    {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052906/Gallery/4_mzppas.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052906/Gallery/3_szo1ot.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052905/Gallery/19_br5eva.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052905/Gallery/21_glkwbr.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052905/Gallery/2_cddjqm.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052905/Gallery/16_mlznvg.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052905/Gallery/18_krtdvj.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052905/Gallery/17_vw6hui.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052904/Gallery/15_fq6muk.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052904/Gallery/12_r8xkyk.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052904/Gallery/14_thoabk.jpg',
+   },
+   {
+      src: 'https://res.cloudinary.com/dhlsvwpny/image/upload/v1683052904/Gallery/13_ufct0z.jpg',
+   }
  ];
 
  export default GalleryImage;
