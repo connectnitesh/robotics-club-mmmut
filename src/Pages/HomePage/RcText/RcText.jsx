b0a7bc4fd8cec970c4cea38dc7104479dfcb7177
connectnitesh <connectnitesh8@gmail.com>
2023-05-03 22:18:50 +0530

    my commit

--- src/Pages/HomePage/RcText/RcText.jsx
+++ src/Pages/HomePage/RcText/RcText.jsx
@@ -4,8 +4,8 @@ import "./RctextStyle.css";
 const RcText = () => {
   return (
     <div className="rctext-container">
-      <h1 className="heading">Robotics Club </h1>
-      <h5 className="heading sm-heading">MMMUT Gorakhpur</h5>
+      <h1 className="rctext-heading">Robotics Club </h1>
+      <h5 className="rctext-heading rctextsm-heading">MMMUT Gorakhpur</h5>
     </div>
   );
 };
